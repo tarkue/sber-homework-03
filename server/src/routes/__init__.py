cdfa290b9fdc31872cdd66ea63da917c77de823f
tarkue <mrfrostlikchannel@gmail.com>
2025-10-18 15:05:05 +0500

    refactor code

--- server/src/routes/__init__.py
+++ server/src/routes/__init__.py
@@ -1,1 +1,7 @@
-from .index import index_router
+from fastapi import APIRouter
+
+index_router = APIRouter()
+
+@index_router.get("/")
+def read_root():
+    return {"message": "Это сообщение было отправлено с сервера, Капитан!"}
--- server/src/routes/index.py
+++
@@ -1,7 +0,0 @@
-from fastapi import APIRouter
-
-index_router = APIRouter(tags=["Root"])
-
-@index_router.get("/")
-def read_root():
-    return {"message": "Это сообщение было отправлено с сервера, Капитан!"}
